94e4c22f8e209786cba5d0acfda8b3dab64c0395
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2018-04-12 15:06:55 +0800

    Fix exception 'ImproperlyConfigured: The included urlconf pbs_project.urls doesn't have any patterns in it'. This exception caused by module 'debug_toolbar' which loaded by import statement in pbs_project/urls.py and try to access urlpatterns in pbs_project/urls.py but at that time, urlpattern is not populated yet.
    the way to reproduce it is
     restart uwsgi server
     access a non exist  url in pbs.

--- pbs_project/urls.py
+++ pbs_project/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls import patterns, include, url
 
-from django_downloadview import ObjectDownloadView
+"""
+some import statmement will load some module (directly or indirectly) 
+and that module maybe include some statement "reverse('djdt:render_panel')" which will access the urlpattern of this module, 
+but urlpatterns are populated after import statement, so this cause famous exception "ImproperlyConfigured: The included urlconf pbs_project.urls doesn't have any patterns in it"
 
+Solution is using two steps to populate urlpatterns
+1. just populate the very basic url patterns, that will make property 'urlpatterns' always avaiable
+2. populate others.
+
+"""
+urlpatterns = patterns('',
+    (r'^docs/', include('django.contrib.admindocs.urls')),
+    (r'^', include('django.contrib.auth.urls'))
+)
+
+from django_downloadview import ObjectDownloadView
 from pbs.document.models import Document
 from pbs.sites import site
 from pbs.forms import PbsPasswordResetForm
@@ -16,15 +30,13 @@ document_download = ObjectDownloadView.as_view(model=Document, file_field='docum
 v1_api = Api(api_name='v1')
 v1_api.register(PrescribedBurnResource())
 
-urlpatterns = patterns('',
-    (r'^docs/', include('django.contrib.admindocs.urls')),
+urlpatterns = urlpatterns + patterns('',
     url(r'^select2/', include('django_select2.urls')),
     (r'^', include('pbs.registration.urls')),
     # the password reset must come before site.urls, site.urls match all
+    (r'^', include(site.urls)),
     url(r'^password_reset/$', 'django.contrib.auth.views.password_reset',
         {'password_reset_form': PbsPasswordResetForm}, name='password_reset'),
-    (r'^', include(site.urls)),
-    (r'^', include('django.contrib.auth.urls')),
     url(r'^chaining/', include('smart_selects.urls')),
     url('^documents/(?P<pk>\d+)/download$', document_download, name='document_download'),
 
