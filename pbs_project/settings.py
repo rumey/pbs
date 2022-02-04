e1444590d6454ca1047a3ebbc45130289e6db39d
Jason Moore <jmoore@austwa.com>
2022-02-04 09:56:41 +0800

    Update settings.py

--- pbs_project/settings.py
+++ pbs_project/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'dpaw_utils.middleware.SSOLoginMiddleware',
+    'pbs.middleware.SSOLoginMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
