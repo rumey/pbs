9749f07e66e6662659fbb347d69acb30254cb67d
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2022-05-27 15:35:48 +0800

    git history v2

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -11,6 +11,7 @@ fi
 
 date_var=$(date +%Y.%m.%d.%H.%M%S)
 BUILD_TAG=dbcawa/pbs:v$date_var
+git log --pretty=medium -30 > ./git_history_recent &&
 docker image build $NO_CACHE --tag $BUILD_TAG . &&
 echo $BUILD_TAG &&
 docker push $BUILD_TAG
