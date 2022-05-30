5b6df32a7f6391c569b9d9e412da5133ee4424e5
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2022-05-30 13:01:16 +0800

    added git history

---
+++ make_docker_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]; then
+    echo "$0 <optional: --no-cache>"
+    exit 1
+fi
+
+if [ $# -eq 1 ]; then
+    NO_CACHE=$1
+fi
+
+date_var=$(date +%Y.%m.%d.%H.%M%S)
+BUILD_TAG=dbcawa/pbs:v$date_var
+docker image build $NO_CACHE --tag $BUILD_TAG . &&
+echo $BUILD_TAG &&
+docker push $BUILD_TAG
